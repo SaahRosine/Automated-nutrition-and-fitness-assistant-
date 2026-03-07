4c32f28bc137e5b9fbd88704992ece37b1137b81
Juan <juanvictorqwerty@gmail.com>
2026-03-07 20:46:56 +0100

    add cors in .env

--- django/_Project/settings.py
+++ django/_Project/settings.py
@@ -142,8 +142,7 @@ REST_FRAMEWORK = {
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
+    config('CORS_ALLOWED_ORIGINS'),
 ]
 
 CORS_ALLOW_CREDENTIALS = True
