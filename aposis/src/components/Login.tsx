e738af20260d676ce001a47305c137b02f929f11
Juan <juanvictorqwerty@gmail.com>
2026-03-08 22:00:22 +0100

    refactoring

--- aposis/src/components/Login.tsx
+++ aposis/src/components/Login.tsx
@@ -1,6 +1,7 @@
 import { useState,lazy, Suspense} from "react";
 import { useNavigate } from "@tanstack/react-router";
 import Cookies from "js-cookie";
+import { buttonStyles, inputStyles, linkStyles } from "#/styles/style";
 
 const ErrorPopup = lazy(() => import("./Error").then(module => ({ default: module.ErrorPopup })));
 
@@ -70,20 +71,18 @@ const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
         </Suspense>
 
     <div
-        className="bg-gray-800 rounded-lg shadow-xl overflow-hidden"
+        className="bg-gray-200 dark:bg-gray-800 rounded-lg shadow-xl overflow-hidden"
     >
         <div className="p-8">
-        <h2 className="text-center text-3xl font-extrabold text-white">
+        <h2 className="text-center text-3xl font-extrabold text-gray-900 dark:text-white">
             Welcome Back
         </h2>
         <p className="mt-4 text-center text-gray-400">Sign in to continue</p>
         <form method="POST" onSubmit={handleSubmit} className="mt-8 space-y-6">
-            <div className="rounded-md shadow-sm">
-            <div>
                 <label className="sr-only" htmlFor="email">Email address</label>
                 <input
                 placeholder="Email address"
-                className="appearance-none relative block w-full px-3 py-3 border border-gray-700 bg-gray-700 text-white rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className={inputStyles}
                 required
                 autoComplete="email"
                 type="email"
@@ -91,12 +90,10 @@ const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
                 onChange={handleEmailChange}
                 id="email"
                 />
-            </div>
-            <div className="mt-4">
                 <label className="sr-only" htmlFor="password">Password</label>
                 <input
                 placeholder="Password"
-                className="appearance-none relative block w-full px-3 py-3 border border-gray-700 bg-gray-700 text-white rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className={inputStyles}
                 required
                 autoComplete="current-password"
                 type="password"
@@ -104,48 +101,29 @@ const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
                 onChange={handlePasswordChange}
                 id="password"
                 />
-            </div>
-            </div>
-
-            <div className="flex items-center justify-between mt-4">
-            <div className="flex items-center">
-                <input
-                className="h-4 w-4 text-indigo-500 focus:ring-indigo-400 border-gray-600 rounded"
-                type="checkbox"
-                name="remember-me"
-                id="remember-me"
-                />
-                <label className="ml-2 block text-sm text-gray-400" htmlFor="remember-me"
-                >Remember me</label
-                >
-            </div>
-
-            <div className="text-sm">
                 <a
-                    className="font-medium text-indigo-500 hover:text-indigo-400"
+                    className={linkStyles}
                     href="/forgot-password"
                 >
                 Forgot your password?
                 </a>
-            </div>
-            </div>
 
-            <div>
             <button
-                className="group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-gray-900 bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className={buttonStyles}
                 type="submit"
                 disabled={loading}
             >
                 Sign In
             </button>
-            </div>
         </form>
         </div>
-        <div className="px-8 py-4 bg-gray-700 text-center">
-        <span className="text-gray-400">Don't have an account?</span>
+        <div className="px-8 py-4 bg-gray-300 dark:bg-gray-700 text-center">
+        <span className="text-gray-600 dark:text-gray-400">Don't have an account?</span>
         <a 
-            className="font-medium text-indigo-500 hover:text-indigo-400" href="/SignUpPage"
-        >Sign up
+            className={linkStyles}
+            href="/SignUpPage"
+        >
+            Sign up
         </a>
         </div>
     </div>
---
+++ aposis/src/styles/style.ts
@@ -0,0 +1,3 @@
+export const inputStyles = "bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm";
+export const buttonStyles = "bg-blue-300 group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-gray-900 dark:bg-indigo-500 dark:hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:bg-gray-400 disabled:cursor-not-allowed block";
+export const linkStyles = "font-medium text-indigo-500 hover:text-indigo-400 block text-sm";
